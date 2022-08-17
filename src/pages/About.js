0b1b54a6986373fc4e9eddee85b4e820537fbbf3
Rawman <rouffaderemi@gmail.com>
2022-08-17 01:00:09 +0100

    cocktail API react.js

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+        exercitationem labore voluptatum, quos aliquid voluptatem quo! Id
+        perferendis numquam quaerat repudiandae officiis deleniti nisi, iure ea
+        mollitia beatae velit? Doloremque cumque et excepturi necessitatibus hic
+        quaerat possimus amet quo optio.
+      </p>
+    </section>
+  );
+};
+
+export default About;
